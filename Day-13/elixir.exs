19f087e6ae13003a5ff29b8637bc39851f8e74cc
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2024-04-14 22:03:14 +0200

    feat(Day13): add part2

--- Day-13/elixir.exs
+++ Day-13/elixir.exs
@@ -29,7 +29,7 @@ defmodule Day13 do
     |> Enum.sum()
   end
 
-  def find_horizontal_reflection_line(_ = %Note{data: data}) do
+  def find_horizontal_reflection_line(_ = %Note{data: data}, remove_smudge \\ false) do
     note_length = length(data)
 
     line_index =
@@ -45,52 +45,77 @@ defmodule Day13 do
 
         part2 = part2 |> Enum.take(min_length) |> Enum.reverse()
 
-        part1 == part2
+        if remove_smudge do
+          char_difference_count(part1 |> Enum.join("\n"), part2 |> Enum.join("\n")) == 1
+        else
+          part1 == part2
+        end
       end)
       |> List.first()
 
     line_index
   end
 
-  def find_vertical_reflection_line(_ = %Note{data: data}) do
-    note_length = length(data)
+  def rotate_note_90(_ = %Note{data: data}) do
+    height = length(data)
+    width = data |> Enum.at(0) |> String.length()
 
-    line_index =
-      data
-      |> Enum.with_index()
-      |> Enum.map(fn {line, _} ->
-        line_length = String.length(line)
-
-        1..(line_length - 1)
-        |> Enum.filter(fn x ->
-          {part1, part2} = String.split_at(line, x)
-
-          min_length = min(String.length(part1), String.length(part2))
-
-          part1 =
-            part1
-            |> String.reverse()
-            |> String.slice(0, min_length)
-
-          part2 = String.slice(part2, 0, min_length)
-
-          part1 == part2
+    new_data =
+      0..(width - 1)
+      |> Enum.map(fn i ->
+        0..(height - 1)
+        |> Enum.map(fn j ->
+          data
+          |> Enum.at(j)
+          |> String.at(i)
         end)
+        |> Enum.join()
       end)
-      |> List.flatten()
-      |> Enum.frequencies()
-      |> Map.to_list()
-      |> Enum.filter(fn {_, count} ->
-        count == note_length
-      end)
-      |> List.first({nil, 0})
-      |> elem(0)
 
-    line_index
+    %Note{data: new_data}
+  end
+
+  def find_vertical_reflection_line(note = %Note{}, remove_smudge \\ false) do
+    note
+    |> rotate_note_90()
+    |> find_horizontal_reflection_line(remove_smudge)
+  end
+
+  def char_difference_count(str1, str2)
+      when is_binary(str1) and is_binary(str2) and str1 == str2 do
+    0
+  end
+
+  def char_difference_count(str1, str2)
+      when is_binary(str1) and is_binary(str2) do
+    if String.length(str1) != String.length(str2) do
+      abs(length(str1) - length(str2))
+    else
+      str2_graphemes = str2 |> String.graphemes()
+
+      str1
+      |> String.graphemes()
+      |> Enum.with_index()
+      |> Enum.reduce(0, fn {char, i}, acc ->
+        if str2_graphemes |> Enum.at(i) == char, do: acc, else: acc + 1
+      end)
+    end
   end
 
   def part2(use_example) do
     input = parse_input(use_example)
+
+    input
+    |> Enum.map(fn note ->
+      h = find_horizontal_reflection_line(note, true)
+      v = find_vertical_reflection_line(note, true)
+
+      v_val = if v == nil, do: 0, else: v
+      h_val = if v != nil or h == nil, do: 0, else: h * 100
+
+      h_val + v_val
+    end)
+    |> Enum.sum()
   end
 
   def parse_input(use_example) do
