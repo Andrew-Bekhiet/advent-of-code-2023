8b0c4aa895240af9f4720a2ed4321757bceb9a77
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2024-06-18 00:59:22 +0300

    feat: Day17 -> part 1

--- Day-17/elixir.exs
+++ Day-17/elixir.exs
@@ -77,86 +77,69 @@ defmodule Day17 do
   def new_dir({x1, y1}, {x2, y2}) when x2 - x1 == 1 and y2 - y1 == 0, do: :right
   def new_dir({x1, y1}, {x2, y2}) when x2 - x1 == -1 and y2 - y1 == 0, do: :left
 
+  def manhattan_distance({x1, y1}, {x2, y2}) do
+    abs(x1 - x2) + abs(y1 - y2)
+  end
+
+  def subtract_dir({x, y}, :up), do: {x, y + 1}
+  def subtract_dir({x, y}, :down), do: {x, y - 1}
+  def subtract_dir({x, y}, :left), do: {x + 1, y}
+  def subtract_dir({x, y}, :right), do: {x - 1, y}
+  def subtract_dir({x, y}, _), do: {x, y}
+
+  def rotate180(:up), do: :down
+  def rotate180(:down), do: :up
+  def rotate180(:left), do: :right
+  def rotate180(:right), do: :left
+  def rotate180(d), do: d
+
   @spec dijkstra(Grid.t(), :gb_sets.set(tuple()), tuple(), map()) :: tuple()
   def dijkstra(
         grid = %Grid{},
-        locations_costs,
+        pq,
         goal,
-        visited_locations \\ %{}
+        visited_locations \\ %{},
+        seen \\ MapSet.new()
       ) do
-    {{hl, dir_len, dir, start, prev}, locations_costs} = :gb_sets.take_smallest(locations_costs)
-
-    visited_locations =
-      visited_locations
-      |> Map.update(start, {prev, dir, hl}, fn
-        {_, _, old_hl} = old when old_hl < hl -> old
-        _ -> {prev, dir, hl}
-      end)
-
-    path = reconstruct_path(visited_locations, {start, dir, hl}, :infinity)
-    IO.puts(grid |> Grid.debug(path))
-
-    dbg(
-      {hl,
-       path
-       |> Map.keys()
-       |> Enum.reject(fn
-         k when is_nil(k) -> true
-         {x, y} -> x == 0 and y == 0
-       end)
-       |> Enum.map(fn l -> grid |> Grid.at(l) end)
-       |> Enum.sum()}
-    )
-
-    IO.puts("")
-    # path = [start | path]
-
-    # path_length = length(path)
-
-    # if Kernel.rem(path_length, 20) == 0 do
-    #   IO.puts(grid |> Grid.debug(path |> MapSet.new()))
-    #   IO.puts("")
-    # end
+    {{hl, start, same_dir_count, prev_dir}, pq} =
+      :gb_sets.take_smallest(pq)
 
     if start == goal do
-      {hl, {start, dir, hl}, visited_locations}
+      {hl, start, visited_locations}
     else
-      # last_4_steps = path |> Enum.take(4)
-      # last_4_steps_len = length(last_4_steps)
-
-      adjacent_locations =
+      {pq, seen} =
         grid
         |> Grid.get_adjacent_locations(start)
-        |> Enum.reject(fn location ->
-          # dir_len >= 4 or
-          Map.has_key?(visited_locations, location) or
-            (dir_len == 3 and new_dir(start, location) == dir)
+        |> Enum.map(fn l -> {l, new_dir(start, l)} end)
+        # |> dbg()
+        |> Enum.reject(fn {to, new_dir} ->
+          rotate180(new_dir) == prev_dir or
+            (new_dir == prev_dir and same_dir_count >= 3) or
+            seen
+            |> MapSet.member?(
+              {to, if(new_dir == prev_dir, do: same_dir_count + 1, else: 1), new_dir}
+            )
         end)
+        |> Enum.reduce({pq, seen}, fn {to, new_dir}, {pq, seen} ->
+          new_hl = hl + Grid.at(grid, to)
 
-      new_locations_costs =
-        adjacent_locations
-        |> Enum.reduce(locations_costs, fn location, new_locations_costs ->
-          if location == {2, 1} and hl == 6 do
-            dbg(location_hl = grid |> Grid.at(location))
-            dbg(location_hl + hl)
-            dbg(new_dir(start, location))
+          if new_dir == prev_dir do
+            {
+              :gb_sets.insert(
+                {new_hl, to, same_dir_count + 1, new_dir},
+                pq
+              ),
+              seen |> MapSet.put({to, same_dir_count + 1, new_dir})
+            }
+          else
+            {
+              :gb_sets.insert({new_hl, to, 1, new_dir}, pq),
+              seen |> MapSet.put({to, 1, new_dir})
+            }
           end
-
-          location_hl = grid |> Grid.at(location)
-          new_hl = location_hl + hl
-
-          new_dir = new_dir(start, location)
-
-          # :gb_sets.filter(fn
-          #   new_locations_costs)
-
-          :gb_sets.add(
-            {new_hl, if(new_dir == dir, do: dir_len + 1, else: 1), new_dir, location, start},
-            new_locations_costs
-          )
         end)
 
-      dijkstra(grid, new_locations_costs, goal, visited_locations)
+      dijkstra(grid, pq, goal, visited_locations, seen)
     end
   end
 
@@ -168,10 +151,10 @@ defmodule Day17 do
     end
   end
 
-  def reconstruct_path(locations_costs, {start, dir, _}, 0), do: []
+  def reconstruct_path(locations_costs, {start, dir, _}, 0), do: %{}
 
   def reconstruct_path(locations_costs, {start, dir, _}, n) when is_integer(n) do
-    with prev when n > 0 <- locations_costs |> Map.get(start) do
+    with prev when n > 0 and not is_nil(prev) <- locations_costs |> Map.get(start) do
       Map.merge(reconstruct_path(locations_costs, prev, n - 1), %{start => dir})
     else
       nil -> %{start => dir}
@@ -179,19 +162,24 @@ defmodule Day17 do
   end
 
   def part1(use_example) do
-    %Grid{width: w, height: h} = input = parse_input(use_example)
+    %Grid{width: w, height: h} = grid = parse_input(use_example)
 
     {hl, start, visited} =
-      dijkstra(
-        input,
-        :gb_sets.from_list([{0, 0, nil, {0, 0}, nil}]),
-        {w - 1, h - 1}
+      grid
+      |> dijkstra(
+        # {heat_loss, distance_to_goal, {x, y} = location, previous_direction, steps_in_same_direction}
+        # {hl, start, same_dir_count, prev_dir}
+        :gb_sets.singleton({0, {0, 0}, 0, nil}),
+        {w - 1, h - 1},
+        %{
+          {0, 0} => {nil, nil, 0}
+        }
       )
 
-    path = reconstruct_path(visited, start, :infinity)
+    path = reconstruct_path(visited, {start, nil, 0}, if(use_example, do: 99, else: 999))
 
     IO.puts(inspect(path, pretty: true))
-    IO.puts(input |> Grid.debug(path))
+    IO.puts(grid |> Grid.debug(path))
 
     IO.puts(
       path
@@ -200,7 +188,7 @@ defmodule Day17 do
         k when is_nil(k) -> true
         {x, y} -> x == 0 and y == 0
       end)
-      |> Enum.map(fn l -> input |> Grid.at(l) end)
+      |> Enum.map(fn l -> grid |> Grid.at(l) end)
       |> Enum.sum()
     )
 
